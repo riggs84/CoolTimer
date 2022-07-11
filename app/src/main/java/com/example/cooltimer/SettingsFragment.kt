f04a3632a9e818f3475b2192e54397c16cf88ded
viktoriurkov <VVYurkov@alfabank.ru>
2022-07-11 19:11:42 +0300

    feat(*): add shared preferences

---
+++ app/src/main/java/com/example/cooltimer/SettingsFragment.kt
@@ -0,0 +1,10 @@
+package com.example.cooltimer
+
+import android.os.Bundle
+import androidx.preference.PreferenceFragmentCompat
+
+class SettingsFragment: PreferenceFragmentCompat() {
+    override fun onCreatePreferences(savedInstanceState: Bundle?, rootKey: String?) {
+        addPreferencesFromResource(R.xml.preferencies)
+    }
+}
